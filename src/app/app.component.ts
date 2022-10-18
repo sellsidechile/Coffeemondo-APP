9d8887d367496a18b25d7f8657b530d2a933ba4d
Matias <matias.peters@sellside.cl>
2022-10-18 20:24:57 -0300

    Cambios sin importancia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { GoogleApiService } from './services/google-api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angularcoffeemondo';
