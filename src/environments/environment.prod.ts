ec454b8b5a88adefe86b430bda5edb18d54444c7
Matias <matias.peters@sellside.cl>
2022-10-31 09:40:53 -0300

    Vinculacion de firebase con nuevo proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -16,11 +16,7 @@ export const environment = {
   production: true,
   firebaseConfig: {
     apiKey: "AIzaSyAp5D6hgldoFCggq7c2sj5_d9LuRGHQFvc",
-<<<<<<< HEAD
-    authDomain: "coffeemondo.app",
-=======
     authDomain: "coffeemondo-365813.firebaseapp.com",
->>>>>>> ab2456edfa32b14e60c2f46651d85f24052cfe29
     projectId: "coffeemondo-365813",
     storageBucket: "coffeemondo-365813.appspot.com",
     messagingSenderId: "661225577781",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,9 @@
 
 export const environment = {
   production: true,
-<<<<<<< HEAD
-  firebaseConfig: {
-    apiKey: "AIzaSyAp5D6hgldoFCggq7c2sj5_d9LuRGHQFvc",
-    authDomain: "coffeemondo.app",
-=======
   firebaseConfig : {
     apiKey: "AIzaSyAp5D6hgldoFCggq7c2sj5_d9LuRGHQFvc",
     authDomain: "coffeemondo-365813.firebaseapp.com",
->>>>>>> ab2456edfa32b14e60c2f46651d85f24052cfe29
     projectId: "coffeemondo-365813",
     storageBucket: "coffeemondo-365813.appspot.com",
     messagingSenderId: "661225577781",
