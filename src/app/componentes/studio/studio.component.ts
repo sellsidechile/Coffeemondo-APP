9cbe69240be8c8996096c315af5be666cc0e4e0d
Brayansellside <brayan.bahamondes@sellside.cl>
2022-11-21 17:42:09 -0300

    navbar y sidebar en pagina studio

--- src/app/componentes/studio/studio.component.ts
+++ src/app/componentes/studio/studio.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Usuarios } from 'src/app/usuarios.model';
+import { UsuariosService } from '../../services/usuarios.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-studio',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./studio.component.scss']
 })
 export class StudioComponent implements OnInit {
-
-  constructor() { }
+  Usuarios: Usuarios[]
+  menuStatus: boolean= false;
+  @Input() sideNavStatus: boolean = true;
+  @Output() sideNavToggled = new EventEmitter<boolean>();
+  constructor(
+    private usuariosService : UsuariosService,
+    private router : Router
+    ){ }
 
   ngOnInit(): void {
-  }
+    document.getElementsByClassName('modal-backdrop')[0].classList.remove('modal-backdrop')
 
+  }
+  sideNavToggle() {
+    this.menuStatus = !this.menuStatus;
+    this.sideNavToggled.emit(this.menuStatus);
+  }
+  
+  onClick(){
+    this.usuariosService.logout()
+      .then(()=>{
+        Swal.fire({
+          title:"¿Estas seguro?",
+          text:"Si cierra sesion va a tener que volver a identificarse.",
+          icon:"warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Si, estoy seguro."
+        }).then( (result)=> {
+          if (result.isConfirmed) {
+            this.router.navigate(['/index']);
+          }
+          
+        })
+      })
+      .catch( () => {});
+  }
 }
+
+
+
